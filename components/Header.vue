bd9b64964ee710c3d1545fb700e61875a5b426d6
G8tekeeper <G8tekeeper@gmail.com>
2021-11-18 11:07:47 +0900

    First commit

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b-navbar class="header" togglb-navbareable="lg" type="dark">
+            <b-navbar-brand href="#" class="title">Butterfly.Net</b-navbar-brand>
+        </b-navbar>
+    </div>
+</template>
+
+<style>
+    .header {
+        background-color: #00BB00;
+        display: block;
+        text-align: center;
+        height: 80px;
+    }
+
+    .title {
+        font-size: 40px;
+    }
+</style>
