55ca6c572960db193c15659d4989feec783d3501
G8tekeeper <G8tekeeper@gmail.com>
2022-01-05 08:57:12 +0900

    added <li> including v-if

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="nav-bar">
         <ul class="nav-list">
-            <li class="nav-item cursor" v-if="$auth.loggedIn">
-                <span class="nav-link">
-                    <b-icon icon="door-closed"></b-icon>
-                    <span class="link-text" @click="$auth.logout()">logout</span>
-                </span>
+            <li v-if="$auth.loggedIn" class="nav-item cursor">
+                <div class="nav-link" @click="$auth.logout()">
+                    <b-icon icon="door-open"></b-icon>
+                    <span class="link-text">logout</span>
+                </div>
             </li>
             <li class="nav-item" v-for="link in links" :key="link.id">
                 <nuxt-link :to="link.url" class="nav-link">
@@ -23,24 +23,16 @@
             return {
                 links: this.$auth.loggedIn ? [
                     { id:1, icon:"journal-bookmark", text:"Description", url:"/"},
-                    { id:2, icon:"pencil-square", text:"Regist URL", url:"regist"},
+                    { id:2, icon:"pencil-square", text:"Regist URL", url:"RegistURL"},
                 ] : [
                     { id:1, icon:"door-open", text:"Login", url:"login"},
                     { id:2, icon:"person-circle", text:"Regist User", url:"RegistUser"},
-                    { id:3, icon:"pencil-square", text:"Regist URL", url:"regist"},
+                    { id:3, icon:"pencil-square", text:"Regist URL", url:"RegistURL"},
                 ],
             };
         },
 
         methods: {
-            addClass() {
-                this.isActive = true
-            },
-
-            removeClass() {
-                this.isActive = false
-            },
-
             async logout() {
                 try {
                     this.$axios.$post('/logout')
@@ -56,10 +48,10 @@
 
 <style>
     :root {
-    --active-color: white;
-    --link-text-color: #00BB00;
-    --nav-background-color: white;
-    --active-background-color: #00BB00;
+        --active-color: white;
+        --link-text-color: #00BB00;
+        --nav-background-color: white;
+        --active-background-color: #00BB00;
     }
 
     .nav-bar {
@@ -68,47 +60,48 @@
     }
 
     .nav-list {
-    padding: 0;
-    margin: 0;
-    position: relative;
-    background-color: var(--nav-background-color);
-    display: inline-flex;
-    border: solid 0.1rem #00BB00;
-    border-radius: 4px;
-    list-style-type: none;
-    overflow: hidden;
+        padding: 0;
+        margin: 0 auto;
+        position: relative;
+        background-color: var(--nav-background-color);
+        display: flex;
+        border: solid 0.1rem #00BB00;
+        border-radius: 4px;
+        list-style-type: none;
+        overflow: hidden;
+        justify-content: space-between;
+        width: 387px;
     }
 
     .nav-item {
-    display: inline-flex;
+        display: flex;
     }
 
     .nav-link {
-    padding: 1.1rem 1rem;
-    display: inline-flex;
-    align-items: center;
-    color: var(--link-text-color);
-    text-decoration: none;
+        padding: 1.1rem 1rem;
+        display: flex;
+        align-items: center;
+        color: var(--link-text-color);
+        text-decoration: none;
     }
 
     .nav-link:hover,
     .nav-link.active {
-    color: var(--active-color);
-    background-color: var(--active-background-color);
+        color: var(--active-color);
+        background-color: var(--active-background-color);
     }
 
-
     .b-icon {
-    height: 2.4rem;
-    width: 2.4rem;
-    justify-content: center;
-    align-items: center;
-    display: inline-flex;
-    margin-right: 0.32rem;
+        height: 2.4rem;
+        width: 2.4rem;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        margin-right: 0.32rem;
     }
 
     .link-text {
-    font-size: 1.76rem;
+        font-size: 1.76rem;
     }
 
 </style>
