fb07b56b24a88f1db7faabf7aad4214000c10902
G8tekeeper <G8tekeeper@gmail.com>
2022-01-05 09:02:18 +0900

    first commit

---
+++ components/Bookmark.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="parent" class="post-footer">
+        <b-icon v-if="isFill" icon="heart-fill" class="heart-icon mt-4 cursor" @click="change"></b-icon>
+        <b-icon v-else icon="heart" class="heart-icon mt-4 cursor" @click="change"></b-icon>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isFill: false,
+            }
+        },
+        methods: {
+            change() {
+                this.isFill = !this.isFill
+            }
+        }
+    }
+</script>
