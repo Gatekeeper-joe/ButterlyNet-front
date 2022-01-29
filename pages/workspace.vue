f555cd92c238c2e7a0cba9c82a413cd220649218
G8tekeeper <G8tekeeper@gmail.com>
2022-01-30 06:10:25 +0900

    Deleted class='section' due to changing the policy of project.

--- pages/workspace.vue
+++ pages/workspace.vue
@@ -1,77 +1,33 @@
 <template>
-    <div class="col-md-9">
-        <div class="home">
-            <Navbar />
-            <div class="secion py-3">
-                <div class="a-header">
-                    <h1>Hot News</h1>
-                </div>
-                <div class="post-list pt-5 d-flex">
-                    <article class="post-card col-md-4" v-for="item in items" :key="item.id">
-                        <div class="post-thumbnail">
-                            <!-- <img :src="item.thumbnail" alt=""> -->
-                            <img src="~/assets/img/MettaurEXE.png" alt="">
-                        </div>
-                        <div class="post-body">
-                            <div class="post-meta">
-                                <b-icon icon="calendar3"></b-icon>
-                                <span>{{ item.date }}</span>
-                            </div>
-                            <div class="post-title">
-                                <span>{{ item.title }}</span>
-                            </div>
-                        </div>
-                        <Bookmark />
-                    </article>
+    <div class="workspace">
+        <Navbar />
+        <div class="a-dashboard d-flex mt-5">
+            <div class="col-md-7">
+                <span class="mt-3">reserved</span>
+            </div>
+            <div class="col-md-6">
+                <div class="h-table-content">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <th>Updated pages</th>
+                                <th>Updated time</th>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>www.google.com</td>
+                                <td>2022/1/29 00:00:00</td>
+                            </tr>
+                                                        <tr>
+                                <td>1</td>
+                                <td>www.google.com</td>
+                                <td>2022/1/29 00:00:00</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="secion py-3">
-                <div class="a-header">
-                    <h1>Unread News</h1>
-                </div>
-                <div class="post-list pt-5 d-flex">
-                    <article class="post-card col-md-4" v-for="item in items" :key="item.id">
-                        <div class="post-thumbnail">
-                            <!-- <img :src="item.thumbnail" alt=""> -->
-                            <img src="~/assets/img/MettaurEXE.png" alt="">
-                        </div>
-                        <div class="post-body">
-                            <div class="post-meta">
-                                <b-icon icon="calendar3"></b-icon>
-                                <span>{{ item.date }}</span>
-                            </div>
-                            <div class="post-title">
-                                <span>{{ item.title }}</span>
-                            </div>
-                        </div>
-                        <Bookmark />
-                    </article>
-                </div>
-            </div>
-            <div class="secion py-3">
-                <div class="a-header">
-                    <h1>My Favorite</h1>
-                </div>
-                <div class="post-list pt-5 d-flex">
-                    <article class="post-card col-md-4" v-for="item in items" :key="item.id">
-                        <div class="post-thumbnail">
-                            <!-- <img :src="item.thumbnail" alt=""> -->
-                            <img src="~/assets/img/MettaurEXE.png" alt="">
-                        </div>
-                        <div class="post-body">
-                            <div class="post-meta">
-                                <b-icon icon="calendar3"></b-icon>
-                                <span>{{ item.date }}</span>
-                            </div>
-                            <div class="post-title">
-                                <span>{{ item.title }}</span>
-                            </div>
-                        </div>
-                        <Bookmark />
-                    </article>
-                </div>
-            </div>
-
         </div>
     </div>
 </template>
@@ -96,52 +52,76 @@
 
         data() {
             return {
-                message: '',
-
-                items: [
-                    {id:1, date:"2021/12/28", thumbnail:"@/assets/img/MettaurEXE.png", title:"脆弱性対応パッチがリリースされました"},
-                    {id:2, date:"2021/12/28", thumbnail:"~/assets/img/MettaurEXE.png", title:"セキュリティリサーチャーにより、新たなPOCが公開されました"},
-                    {id:3, date:"2021/12/28", thumbnail:"~/assets/img/MettaurEXE.png", title:"株式会社Jがランサムウェア被害"},
-                ]
             }
         },
 
-        beforeRouteEnter(to, from, next) {
-            next(vm => {
-                vm.prevRoute = from;
-            })
-        },
+        // beforeRouteEnter(to, from, next) {
+        //     next(vm => {
+        //         vm.prevRoute = from;
+        //     })
+        // },
 
-        mounted() {
-            if (this.prevRoute.path === '/registUser') {
-                this.message = 'ユーザ登録が完了しました。' + "\n" + '最高の体験があなたを待っています(小並感)';
-                alert(this.message);
-            }
-        },
+        // mounted() {
+        //     if (this.prevRoute.path === '/registUser') {
+        //         this.message = 'ユーザ登録が完了しました。' + "\n" + '最高の体験があなたを待っています(小並感)';
+        //         alert(this.message);
+        //     }
+        // },
     }
 </script>
 
 <style>
-    .a-header {
-        border-bottom: 0.2rem solid;
+    table {
+        width: auto;
+        border-spacing: 0;
+        font-size:14px;
     }
-
-    .post-list > article {
-        margin-bottom: 1.6rem;
-        display: flex;
-        flex-direction: column;
+    table th {
+        color: #fff;
+        padding: 8px 15px;
+        background: #258;
+        background:-moz-linear-gradient(rgba(34,85,136,0.7), rgba(34,85,136,0.9) 50%);
+        background:-webkit-gradient(linear, 100% 0%, 100% 50%, from(rgba(34,85,136,0.7)), to(rgba(34,85,136,0.9)));
+        font-weight: normal;
+        border-left:1px solid #258;
+        border-top:1px solid #258;
+        border-bottom:1px solid #258;
+        line-height: 100%;
+        text-align: center;
+        text-shadow:0 -1px 0 rgba(34,85,136,0.9);
+        box-shadow: 0px 1px 1px rgba(255,255,255,0.3) inset;
     }
-
-    .post-title {
-        height: 7rem;
+    table th:first-child {
+        border-radius: 5px 0 0 0;
     }
-
-    .post-footer {
-        border-top: solid 1px;
-        text-align: right;
+    table th:last-child {
+        border-radius:0 5px 0 0;
+        border-right:1px solid #258;
+        box-shadow: 2px 2px 1px rgba(0,0,0,0.1),0px 1px 1px rgba(255,255,255,0.3) inset;
     }
-
-    .heart-icon {
-        color: #FF69A3;
+    table tr td {
+        padding: 5px 40px;
+        border-bottom: 1px solid #84b2e0;
+        border-left: 1px solid #84b2e0;
+        text-align: center;
+    }
+    table tr td:last-child {
+        border-right: 1px solid #84b2e0;
+        box-shadow: 2px 2px 1px rgba(0,0,0,0.1);
+    }
+    table tr {
+        background: #fff;
+    }
+    table tr:nth-child(2n+1) {
+        background: #f1f6fc;
+    }
+    table tr:last-child td {
+        box-shadow: 2px 2px 1px rgba(0,0,0,0.1);
+    }
+    table tr:last-child td:first-child {
+        border-radius: 0 0 0 5px;
+    }
+    table tr:last-child td:last-child {
+        border-radius: 0 0 5px 0;
     }
 </style>
