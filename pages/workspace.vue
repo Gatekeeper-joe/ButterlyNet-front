0f763aec4e28628a20c41adf4f9804515f3deefe
G8tekeeper <G8tekeeper@gmail.com>
2022-02-21 15:14:56 +0900

    Deleted rows that have feature of importing CDN.

--- pages/workspace.vue
+++ pages/workspace.vue
@@ -14,8 +14,6 @@
     </div>
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
 <script>
     const axios = require('axios')
     import Navbar from "~/components/Navbar"
