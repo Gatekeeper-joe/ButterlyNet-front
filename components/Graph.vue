7837dc9dc7b34e6c7c61a720828630ff33c07dab
G8tekeeper <G8tekeeper@gmail.com>
2022-03-02 07:30:29 +0900

    First commit.

---
+++ components/Graph.vue
@@ -0,0 +1,33 @@
+<script>
+    import { Doughnut } from "vue-chartjs"
+
+    export default ({
+        extends: Doughnut,
+        data() {
+            return {
+            graphData: {
+                labels: ['Open', 'Pending', 'Close'],
+                datasets: [
+                {
+                    data: [3, 2, 5],
+                    backgroundColor: ['#F44336', '#FF9800', '#9E9E9E']
+                }
+                ]
+            },
+            options: {
+                responsive: true
+            }
+            }
+        },
+        mounted() {
+            this.renderChart(this.graphData, this.options)
+        }
+    })
+</script>
+
+<style>
+    #doughnut-chart {
+        width: 400px !important;
+        height: 400px !important;
+    }
+</style>
