32e108623d42b700bda129249ef904db55b01959
G8tekeeper <G8tekeeper@gmail.com>
2022-03-07 20:39:33 +0900

    Was Graph.vue.

---
+++ components/Chart.vue
@@ -0,0 +1,27 @@
+<script>
+    import { Doughnut } from "vue-chartjs"
+
+    export default ({
+        extends: Doughnut,
+        props: ['chartData'],
+
+        data () {
+            return {
+                options: {
+                    responsive: true
+                }
+            }
+        },
+
+        mounted () {
+            this.renderChart(this.chartData, this.options)
+        }
+    })
+</script>
+
+<style>
+    #doughnut-chart {
+        width: 400px !important;
+        height: 400px !important;
+    }
+</style>
