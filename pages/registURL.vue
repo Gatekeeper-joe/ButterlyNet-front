74af198c42d352fe43c5a9f4e78f49a0436d58a3
G8tekeeper <G8tekeeper@gmail.com>
2022-03-09 15:36:34 +0900

    Edited a text on button.

--- pages/registURL.vue
+++ pages/registURL.vue
@@ -28,7 +28,7 @@
                     <div class="form-group row mb-0 form-lower-part">
                         <div class="col-md-12 text-center">
                             <button type="submit" class="btn btn-primary">
-                                Regist
+                                Submit
                             </button>
                         </div>
                     </div>
